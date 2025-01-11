f0037bf8a0ad35f1466d23cd89053295b8a1c96b
SangHyun Byun <101382267+ShByeon3968@users.noreply.github.com>
2025-01-11 16:48:32 +0900

    Create base.py

---
+++ base.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+
+class Function:
+    def __call__(self, input: Variable):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    
+    def forward(self,x):
+        raise NotImplementedError()
+    
+    def backward(self,gy):
+        raise NotImplementedError()
+
+# 제곱 연산
+class Square(Function):
+    def forward(self,x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+    
+# Napier's Constant
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+# 중앙 차분
+def numerical_diff(f:Function, x:Variable, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
